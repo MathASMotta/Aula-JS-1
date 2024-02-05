cd31b573d3347ad2fd24e0ee01e6a66c4634ae30
MathASMotta <rm550352@fiap.com.br>
2024-02-05 17:47:31 -0300

    Code Added

---
+++ aula 1 (do zero)/script.js
@@ -0,0 +1,65 @@
+const botaoAnterior = document.getElementById('anterior');
+const botaoPlayPause = document.getElementById('play-pause');
+const botaoProximo = document.getElementById('proximo');
+const nomeCapitulo = document.getElementById('capitulo');
+const audioCapitulo = document.getElementById('audio-capitulo');
+
+const numeroCapitulos = 10;
+let taTocando = 0;
+let capituloAtual = 1;
+
+function tocarFaixa() {
+    audioCapitulo.play();
+    botaoPlayPause.classList.remove('bi-play-circle-fill');
+    botaoPlayPause.classList.add('bi-pause-circle-fill');
+}
+
+function pausarFaixa() {
+    audioCapitulo.pause();
+    botaoPlayPause.classList.remove('bi-pause-circle-fill');
+    botaoPlayPause.classList.add('bi-play-circle-fill');
+}
+
+function tocarOuPausar() {
+    if ( taTocando === 0 ) {
+        tocarFaixa();
+        taTocando = 1;
+    } else {
+        pausarFaixa();
+        taTocando = 0;
+    }
+}
+
+function trocarNomeFaixa() {
+    nomeCapitulo.innerText = 'Capítulo ' + capituloAtual;
+}
+
+function proximaFaixa() {
+    if ( capituloAtual === numeroCapitulos ) {
+        capituloAtual = 1;
+    } else {
+        capituloAtual = capituloAtual + 1;
+    }
+
+    audioCapitulo.src = "./books/dom-casmurro/" + capituloAtual + ".mp3";
+    tocarFaixa();
+    taTocando = 1;
+    trocarNomeFaixa();
+}
+
+function voltarFaixa() {
+    if ( capituloAtual === numeroCapitulos ) {
+        capituloAtual = 10;
+    } else {
+        capituloAtual = capituloAtual - 1;
+    }
+
+    audioCapitulo.src = "./books/dom-casmurro/" + capituloAtual + ".mp3";
+    tocarFaixa();
+    taTocando = 1;
+    trocarNomeFaixa();
+}
+
+botaoAnterior.addEventListener('click', voltarFaixa);
+botaoPlayPause.addEventListener('click', tocarOuPausar);
+botaoProximo.addEventListener('click', proximaFaixa);
